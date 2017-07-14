c0d27eeeed78c0a3518122ede8f50d562ab4c0ef
brainwipe <brainwiped@gmail.com>
2017-07-14 09:57:25 +0100

    Simplification of the script

--- bash/gitter.sh
+++ bash/gitter.sh
@@ -4,33 +4,14 @@ repository=$(basename "$url" ".${url##*.}")
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo -e "You are comitting to \e[92m${repository}\e[39m on branch \e[92m${branch}\e[39m"
-echo "Does your commit relate to a ticket? (y/n)"
-read hasTicket
 
-while [ "$hasTicket" != "y" -a "$hasTicket" != "Y" -a "$hasTicket" != "n" -a "$hasTicket" != "N" ]; do
-  echo "Please enter either y or n"
-  read hasTicket
-done
+echo "Please enter the Jira ticket name"
+read issueName
 
-if [ "$hasTicket" == "y" -o "$hasTicket" == "Y" ]; then
-  echo "Please enter the issue name"
-  read issueName
-fi
-
-if [ ! -z "$issueName" ]; then
-  ((messageLimit = 49 - ${#issueName}))
-else
-  messageLimit=50
-fi
-
-echo "Please enter your commit message ( ${messageLimit} character limit)"
+echo "Please enter your commit message"
 read commitMessage
 
-if [ ! -z "$issueName" ]; then
-  fullMessage="${issueName} ${commitMessage}"
-else
-  fullMessage="${commitMessage}"
-fi
+fullMessage="${issueName} ${commitMessage}"
 
 echo "Committing and pushing all changes to git with message: ${fullMessage}"
 
