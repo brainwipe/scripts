32bcb4d8479ba8e57cbf1ab9d2e40c5c96c56a5b
Rob Lang <brainwiped@gmail.com>
2019-10-02 10:32:51 +0100

    Updated solarized to work with latest version of windows

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,12 @@
 # Sets the default Powershell Command Prompt as: [time] currentdir >
 # and clears the window.
 # Put in %UserProfile%\Documents\WindowsPowerShell\
-# https://github.com/brainwipe/Scripts
+# https://github.com/brainwipe/scripts
 
 function Prompt {
   # Print the current time:
   Write-Host ("[") -nonewline
-  Write-Host (Get-Date -format HH:mm:ss) -nonewline
+  Write-Host (Get-Date -format HH:mm.ss) -nonewline
   Write-Host ("] ") -nonewline
   # Print the working directory:
   $p = Split-Path -leaf -path (Get-Location)
@@ -15,16 +15,17 @@ function Prompt {
   return " ";
 }
 
-
 Import-Module PSReadLine
 
-
 # Import all the functions in the Network module in ./Modules/Network.psm1
-#Import-Module Network
+Import-Module Network -DisableNameChecking
 
-# Set Solarize Dark
-. (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(switch($HOST.UI.RawUI.BackgroundColor.ToString()){'White'{'Set-SolarizedLightColorDefaults.ps1'}'Black'{'SolarizeTheme/Set-SolarizedDarkColorDefaults.ps1'}default{return}}))
+$env:PSModulePath = $env:PSModulePath + ";c:\Projects\MCO-PowershellCore\src"
+Import-Module MCO -DisableNameChecking
+
+. (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath 'SolarizeTheme/Set-SolarizedDarkColorDefaults.ps1')
 
 # Move to C: and clear the screen
 cd \
-Clear-Host
+cls
+
